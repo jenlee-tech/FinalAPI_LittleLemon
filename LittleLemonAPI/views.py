9d00ba4536fa19237469dc4145fd7cb417f95ab7
Jennifer Lee <jenlee.tech@gmail.com>
2024-01-29 23:04:44 -0500

    implemented pagination and query params for it

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-# from rest_framework import viewsets
 from .models import MenuItem, Category
 from .serializers import MenuItemSerializer, CategoryItemsSerializer
 from rest_framework import generics
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage
 
 
 class MenuItemsViewSet(generics.ListCreateAPIView):
@@ -12,11 +12,16 @@ class MenuItemsViewSet(generics.ListCreateAPIView):
 
     def get(self, request, *args, **kwargs):
         if (request.method == 'GET'):
+            # example query strings - this is for api/menu-items?category=Appetizer
             items = MenuItem.objects.select_related('category').all()
             category_name = request.query_params.get('category')
             to_price = request.query_params.get('to_price')
             search = request.query_params.get('search')
             ordering = request.query_params.get('ordering')
+
+            perpage = request.query_params.get('perpage', default=2)
+            page = request.query_params.get('page', default=1)
+
             if category_name:
                 items = items.filter(category__title=category_name)
             if to_price:
@@ -28,6 +33,13 @@ class MenuItemsViewSet(generics.ListCreateAPIView):
                 items = items.order_by(*ordering_fields)
                 # this is for api/menu-items?ordering=price,title (order by price as an example)
 
+            paginator = Paginator(items, per_page=perpage)
+            try:
+                items = paginator.page(number=page)
+            except EmptyPage:
+                items = []
+
+            # preparing response based on above code
             serialized_item = MenuItemSerializer(items, many=True)
             return Response(serialized_item.data)
 
