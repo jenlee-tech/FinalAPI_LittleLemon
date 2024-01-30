4b404221ab71db970593669385b2c48170ad80ea
Jennifer Lee <jenlee.tech@gmail.com>
2024-01-29 22:52:37 -0500

    implemented ordering by more than one field

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -16,12 +16,17 @@ class MenuItemsViewSet(generics.ListCreateAPIView):
             category_name = request.query_params.get('category')
             to_price = request.query_params.get('to_price')
             search = request.query_params.get('search')
+            ordering = request.query_params.get('ordering')
             if category_name:
                 items = items.filter(category__title=category_name)
             if to_price:
                 items = items.filter(price__lte=to_price)
             if search:
                 items = items.filter(title__icontains=search)
+            if ordering:
+                ordering_fields = ordering.split(",")
+                items = items.order_by(*ordering_fields)
+                # this is for api/menu-items?ordering=price,title (order by price as an example)
 
             serialized_item = MenuItemSerializer(items, many=True)
             return Response(serialized_item.data)
