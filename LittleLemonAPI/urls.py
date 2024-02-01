f4a156d512b015f28e863d2d8db927b4ca400184
Jennifer Lee <jenlee.tech@gmail.com>
2024-01-31 23:03:13 -0500

    implemented functionality in creating tokens at an endpoint

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     # path('menu-items/', views.MenuItemsViewSet.as_view()),
     # path('menu-items/<int:pk>', views.SingleMenuItemViewSet.as_view()),
@@ -12,5 +14,7 @@ urlpatterns = [
     path('cart/menu-items/<int:pk>', views.CartItemViewSet.as_view()),
     path('orders/<int:pk>', views.OrderViewSet.as_view()),
     path('orderitem/<int:pk>', views.OrderItemViewSet.as_view()),
-    path('secret', views.secret)
+    path('secret', views.secret),
+    # this will help user generate a token at this endpoint
+    path('api-token-auth/', obtain_auth_token)
 ]
