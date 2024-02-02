33d4db089b98c50dcc07fcd72a9d0b1800fabdde
Jennifer Lee <jenlee.tech@gmail.com>
2024-02-02 17:12:58 -0500

    added JWT blacklist feature

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'djoser',
     'django_filters',
     'rest_framework_simplejwt',
-    # 'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
--- Littlelemon/urls.py
+++ Littlelemon/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(),
+         name='token_blacklist'),
 ]
