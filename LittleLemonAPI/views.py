f65e5fcb2f65b6794fbe6bcbd1c99009b602e8bc
Jennifer Lee <jenlee.tech@gmail.com>
2024-02-13 14:31:36 -0500

    created Post function for customer order

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -180,11 +180,16 @@ class OrderViewSet(generics.ListCreateAPIView):
             serializer = OrderSerializer(orders, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def get(self, request, *args, **kwargs):
-    #     if (request.method == 'GET'):
-    #         # example query strings - this is for api/menu-items?category=Appetizer
-    #         items = OrderItem.objects.all()
-    #         return Response(items, status=status.HTTP_200_OK)
+    def post(self, request):
+        if request.user.groups.filter(name="Customer").exists():
+            serializer = OrderSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("Hello! - Not authorized to post for this customer...", status=status.HTTP_400_BAD_REQUEST)
 
 
 class OrderItemViewSet(generics.RetrieveUpdateAPIView):
