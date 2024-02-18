00f15c198203bbb024a1f88196c66beb6bf48b4f
Jennifer Lee <jenlee.tech@gmail.com>
2024-02-17 23:17:35 -0500

    change url pattern in order for post to work properly

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items/',
-         views.MenuItemsViewSet.as_view({'get': 'list', 'post': 'create'})),
+         views.MenuItemsViewSet.as_view({'get': 'list', 'post': 'post'})),
     path('menu-items/<int:pk>',
          views.SingleMenuItemViewSet.as_view()),
     path('categories/', views.CategoryItemsView.as_view()),
